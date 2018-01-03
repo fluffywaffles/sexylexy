29100c05336f28aab65179debedb98fd87d65f1f
fluffywaffles <skorlir@gmail.com>
2018-01-03 10:40:26 -0600

    Refactor/Major: stop parsing numbers into floats

--- lex.js
+++ lex.js
@@ -21,9 +21,6 @@ function lex (src, dst=[]) {
   if (hint === '"') {
     const [ string, remainder ] = lex_string(src)
     return lex(remainder, dst.concat([ string ]))
-  } else if (/\d/.test(hint)) {
-    const [ number, remainder ] = lex_number(src)
-    return lex(remainder, dst.concat([ number ]))
   } else {
     const [ atom, remainder ] = lex_atom(src)
     return lex(remainder, dst.concat([ atom ]))
@@ -41,11 +38,6 @@ function lex_string (src) {
   const body_length = rquote_re_result.index
   const string_length = body_length + 2 // add back the quotes
   return [ src.slice(0, string_length), src.substr(string_length) ]
-}
-
-function lex_number (src) {
-  const [ number_string, remainder ] = token(src)
-  return [ parseFloat(number_string), remainder ]
 }
 
 function token (src) {
@@ -66,13 +58,24 @@ export function test (suite) {
     t => t.eq(eat_spaces('    a'))('a'),
     t => t.eq(lex_atom('apple'))([ 'apple', '' ]),
     t => t.eq(lex_string('"hi there"'))([ '"hi there"', '' ]),
-    t => t.eq(lex_number('512.16'))([ 512.16, '' ]),
+    t => t.eq(lex_atom('512.16'))([ '512.16', '' ]),
     t => t.eq(token('apple banana'))([ 'apple', ' banana' ]),
     t => t.eq(token('(hello 5 )'))([ '(hello', ' 5 )' ]),
     t => t.eq(lex(' )'))([ '', [] ]),
     t => t.eq(lex('(sexp [5 "hello"] (atom a "b" 3 (deeper 14 again)) continues)'))
-             ([[ 'sexp', [ 5, '"hello"' ], [ 'atom', 'a', '"b"', 3, [ 'deeper', 14, 'again' ] ], 'continues' ]]),
-    t => t.eq(lex('55'))([ 55 ]),
-    t => t.eq(lex('123 "abc" hi'))([ 123, '"abc"', 'hi' ]),
+             ([[ 'sexp', [ '5', '"hello"' ], [ 'atom', 'a', '"b"', '3', [ 'deeper', '14', 'again' ] ], 'continues' ]]),
+    t => t.eq(lex('55'))([ '55' ]),
+    t => t.eq(lex('123 "abc" hi'))([ '123', '"abc"', 'hi' ]),
+    t => t.eq(lex(`(form [] (text [] "hello, kitten")
+                                (list [] [(text [] "hello, kitten") (number [] 14)])
+                                (number [] 14)
+                                (media [application/octet-stream] "0x47ba1399")
+                                (group [] (text [] "hello, kitten")))`.replace(/\n/g, '')))
+            ([[ 'form', [], [ 'text', [], '"hello, kitten"' ],
+                           [ 'list', [], [[ 'text', [], '"hello, kitten"' ],
+                                          [ 'number', [], '14' ]] ],
+                           [ 'number', [], '14' ],
+                           [ 'media', [ 'application/octet-stream' ], '"0x47ba1399"' ],
+                           [ 'group', [], [ 'text', [], '"hello, kitten"' ] ]]])
   ])
 }
