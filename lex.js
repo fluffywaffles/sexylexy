151dc736777bc5307d10f953a4d49d6d3a3d5347
fluffywaffles <skorlir@gmail.com>
2018-01-31 17:53:36 -0600

    Minor: tweaks and tests

--- lex.js
+++ lex.js
@@ -1,4 +1,4 @@
-// NOTE: if we want to detect imbalanced parens we need to analyze ahead-of-time
+// TODO: detect imbalanced parens by analysis ahead of time
 function lex (src, dst=[]) {
   if (src === '') return dst
 
@@ -19,19 +19,15 @@ function lex (src, dst=[]) {
 
   // data
   if (hint === '"') {
-    const [ string, remainder ] = lex_string(src)
+    const [ string, remainder ] = string(src)
     return lex(remainder, dst.concat([ string ]))
   } else {
-    const [ atom, remainder ] = lex_atom(src)
+    const [ atom, remainder ] = atom(src)
     return lex(remainder, dst.concat([ atom ]))
   }
 }
 
-function lex_atom (src) {
-  return token(src)
-}
-
-function lex_string (src) {
+function string (src) {
   const no_lquote = src.substr(1)
   const rquote_re_result = /"/.exec(no_lquote)
   if (rquote_re_result === null) throw new Error(`non-terminated string`)
@@ -40,7 +36,7 @@ function lex_string (src) {
   return [ src.slice(0, string_length), src.substr(string_length) ]
 }
 
-function token (src) {
+function atom (src) {
   const space_re_result = /\s|\)|\]/.exec(src)
   const end_index = space_re_result != null ? space_re_result.index : src.length
   return [ src.slice(0, end_index), src.substr(end_index) ]
@@ -56,16 +52,18 @@ export default lex
 export function test (suite) {
   suite(`test`, [
     t => t.eq(eat_spaces('    a'))('a'),
-    t => t.eq(lex_atom('apple'))([ 'apple', '' ]),
-    t => t.eq(lex_string('"hi there"'))([ '"hi there"', '' ]),
-    t => t.eq(lex_atom('512.16'))([ '512.16', '' ]),
-    t => t.eq(token('apple banana'))([ 'apple', ' banana' ]),
-    t => t.eq(token('(hello 5 )'))([ '(hello', ' 5 )' ]),
+    t => t.eq(atom('apple'))([ 'apple', '' ]),
+    t => t.eq(string('"hi there"'))([ '"hi there"', '' ]),
+    t => t.eq(atom('512.16'))([ '512.16', '' ]),
+    t => t.eq(atom('apple banana strawberry'))([ 'apple', ' banana strawberry' ]),
+    t => t.eq(atom('(hello 5 )'))([ '(hello', ' 5 )' ]),
     t => t.eq(lex(' )'))([ '', [] ]),
     t => t.eq(lex('(sexp [5 "hello"] (atom a "b" 3 (deeper 14 again)) continues)'))
              ([[ 'sexp', [ '5', '"hello"' ], [ 'atom', 'a', '"b"', '3', [ 'deeper', '14', 'again' ] ], 'continues' ]]),
     t => t.eq(lex('55'))([ '55' ]),
     t => t.eq(lex('123 "abc" hi'))([ '123', '"abc"', 'hi' ]),
+    t => t.eq(lex('"(this expression is quoted)()(([)"'))
+            (['"(this expression is quoted)()(([)"']),
     t => t.eq(lex(`(form [] (text [] "hello, kitten")
                                 (list [] [(text [] "hello, kitten") (number [] 14)])
                                 (number [] 14)
